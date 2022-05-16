8f650b465ab25cb8f0ee6acc8e6478430d9163ae
atlas-techno <atlas.tech.senai@gmail.com>
2022-05-16 23:44:48 +0000

    .

--- app/modules/mongodb.py
+++ app/modules/mongodb.py
@@ -1,7 +1,19 @@
 import pymongo
 from pymongo import MongoClient
+#import urllib.parse
 
-cluster = MongoClient("mongodb+srv://atlas-techno:Atlas@132i@cluster0.wts6q.mongodb.net/?retryWrites=true&w=majority")
-db = cluster["atlas"]
+#username = urllib.parse.quote_plus('Atlas')
+
+#password = urllib.parse.quote_plus('Atlas@132')
+
+cluster = "mongodb+srv://atlas-techno:Atlas132i@cluster0.wts6q.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(cluster)
+
+db = client["atlas"]
+
 collection = db["db"]
 
+data = {"_id":0}
+
+collection.insert_one(data)
